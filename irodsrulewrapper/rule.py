5b145a8fd8fb1a47e32e181a85ddfbb9d1dd86f9
JonathanMELIUS <meliusjonathan@gmail.com>
2021-02-12 12:05:11 +0100

    Add rule methods docstring [DHS-1141]

--- irodsrulewrapper/rule.py
+++ irodsrulewrapper/rule.py
@@ -43,6 +43,14 @@ class RuleManager:
                                         password=os.environ['IRODS_PASS'], zone='nlmumc', client_user=client_user)
 
     def check_irods_connection(self):
+        """
+        Check if an iRODS connection can be established
+
+        Returns
+        -------
+        bool
+            boolean
+        """
         try:
             self.session.pool.get_connection()
         except CAT_INVALID_CLIENT_USER:
@@ -52,27 +60,85 @@ class RuleManager:
 
     @rule_call
     def get_users(self, show_service_accounts):
+        """
+        Get the list of users
+
+        Parameters
+        ----------
+        show_service_accounts : str
+            'true'/'false' excepted values; If true, hide the service accounts in the result
+
+        Returns
+        -------
+        Users
+            dto.Users object
+        """
         if show_service_accounts != "false" and show_service_accounts != "true":
             raise RuleInputValidationError("invalid value for *showServiceAccounts: expected 'true' or 'false'")
         return RuleInfo(name="getUsers", get_result=True, session=self.session, dto=Users)
 
     @rule_call
     def get_groups(self, show_service_accounts):
+        """
+        Get the list of groups
+
+        Parameters
+        ----------
+        show_service_accounts : str
+            'true'/'false' excepted values; If true, hide the special groups in the result
+
+        Returns
+        -------
+        Groups
+            dto.Groups object
+        """
         if show_service_accounts != "false" and show_service_accounts != "true":
             raise RuleInputValidationError("invalid value for *showServiceAccounts: expected 'true' or 'false'")
         return RuleInfo(name="getGroups", get_result=True, session=self.session, dto=Groups)
 
     @rule_call
     def get_data_stewards(self):
+        """
+        Get the list of data stewards
+
+        Returns
+        -------
+        DataStewards
+            dto.DataStewards object
+        """
         return RuleInfo(name="getDataStewards", get_result=True, session=self.session, dto=DataStewards)
 
     @rule_call
     def open_project_collection(self, project, project_collection, user, rights):
+        """
+        Set the ACL of a given collection
+
+        Parameters
+        ----------
+        project : str
+            Project id
+        project_collection : str
+            Collection id
+        user : str
+            The username
+        rights : str
+            access level: 'own', 'write', 'read'
+        """
         # Do input validation here
         return RuleInfo(name="openProjectCollection", get_result=False, session=self.session, dto=None)
 
     @rule_call
     def close_project_collection(self, project, project_collection):
+        """
+        Set the ACL of a given collection
+
+        Parameters
+        ----------
+        project : str
+            Project id
+        project_collection : str
+            Collection id
+        """
         # Do input validation here
         return RuleInfo(name="closeProjectCollection", get_result=False, session=self.session, dto=None)
 
@@ -80,28 +146,143 @@ class RuleManager:
     def create_new_project(self, authorizationPeriodEndDate, dataRetentionPeriodEndDate,
                        ingestResource, resource, storageQuotaGb, title, principalInvestigator,
                        dataSteward, respCostCenter, openAccess, tapeArchive):
-        # Do input validation here
+        """
+        Create a new iRODS project
+
+        Parameters
+        ----------
+        authorizationPeriodEndDate : str
+            The username
+        dataRetentionPeriodEndDate : str
+            The username
+        ingestResource : str
+            The ingest resource to use during the ingestion
+        resource : str
+            The destination resource to store future collection
+        storageQuotaGb  : str
+            The storage quota in Gb
+        title : str
+            The project title
+        principalInvestigator : str
+            The principal investigator(OBI:0000103) for the project
+        dataSteward : str
+            The data steward for the project
+        respCostCenter : str
+            The budget number
+        openAccess : str
+            'true'/'false' excepted values
+        tapeArchive : str
+            'true'/'false' excepted values
+
+        Returns
+        -------
+        CreateProject
+            dto.CreateProject object
+        """
+        # TODO extend parameters validation
+        if openAccess != "false" and openAccess != "true":
+            raise RuleInputValidationError("invalid value for *openAccess: expected 'true' or 'false'")
+
+        if tapeArchive != "false" and tapeArchive != "true":
+            raise RuleInputValidationError("invalid value for *tapeArchive: expected 'true' or 'false'")
         return RuleInfo(name="create_new_project", get_result=True, session=self.session, dto=CreateProject)
 
     @rule_call
     def get_username_attribute_value(self, username, attribute):
-        # Do input validation here
+        """
+        Query an attribute value from the user list of AVU
+
+        Parameters
+        ----------
+        username : str
+            The username
+        attribute : str
+            The user attribute to query
+
+        Returns
+        -------
+        AttributeValue
+            dto.AttributeValue object
+        """
+        if type(username) != str:
+            raise RuleInputValidationError("invalid type for *username: expected a string")
+        if type(attribute) != str:
+            raise RuleInputValidationError("invalid type for *attribute: expected a string")
+
         return RuleInfo(name="get_username_attribute_value", get_result=True, session=self.session, dto=AttributeValue)
 
     @rule_call
     def set_acl(self, mode, access_level, user, path):
-        # Do input validation here
+        """
+        Set the ACL of a given collection
+
+        Parameters
+        ----------
+        mode : str
+            'default', 'recursive' excepted values
+        access_level : str
+            access level: 'own', 'write', 'read'
+        user : str
+            The username
+        path : str
+            The absolute path of the collection
+        """
+        if mode != "default" and mode != "recursive":
+            raise RuleInputValidationError("invalid value for *mode: expected 'default' or 'recursive'")
+        if access_level != "own" and access_level != "write" and access_level != "read":
+            raise RuleInputValidationError("invalid value for *access_level: expected 'default' or 'recursive'")
+        if type(user) != str:
+            raise RuleInputValidationError("invalid type for *user: expected a string")
+        if type(path) != str:
+            raise RuleInputValidationError("invalid type for *path: expected a string")
+
         return RuleInfo(name="set_acl", get_result=False, session=self.session, dto=None)
 
     @rule_call
     def get_ingest_resources(self):
+        """
+        Get the list of ingest resources
+
+        Returns
+        -------
+        Resources
+            dto.Resources object
+        """
+
         return RuleInfo(name="getIngestResources", get_result=True, session=self.session, dto=Resources)
 
     @rule_call
     def get_destination_resources(self):
+        """
+        Get the list of destination resources
+
+        Returns
+        -------
+        Resources
+            dto.Resources object
+        """
+
         return RuleInfo(name="getDestinationResources", get_result=True, session=self.session, dto=Resources)
 
     @rule_call
     def get_user_group_memberships(self, show_special_groups, username):
+        """
+        Get the group membership of a given user
+
+        Parameters
+        ----------
+        show_special_groups : str
+            'true'/'false' excepted values; If true, hide the special groups in the result
+        username : str
+            The username to use for the query
+
+        Returns
+        -------
+        Groups
+            dto.Groups object
+        """
+        if show_special_groups != "false" and show_special_groups != "true":
+            raise RuleInputValidationError("invalid value for *showServiceAccounts: expected 'true' or 'false'")
+
         return RuleInfo(name="get_user_group_memberships", get_result=True, session=self.session, dto=Groups)
 

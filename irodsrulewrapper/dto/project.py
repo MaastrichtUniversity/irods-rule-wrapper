a1f1629611e3397e72568442cf0f1addb519ddfa
Tim <tim@lustberg.nl>
2021-02-17 17:09:26 +0100

    DHS-1144 merged the project objects and fixed the tests

--- irodsrulewrapper/dto/project.py
+++ irodsrulewrapper/dto/project.py
@@ -1,30 +1,56 @@
-from typing import Dict
+from .users import Users
+from .groups import Groups
+from typing import List, Dict
 
 
 class Project:
     def __init__(self,
-                 project: str,
-                 resource: str,
+                 id: str,
                  title: str,
-                 principle_investigator: str,
+                 enable_open_access_export: bool,
+                 enable_archive: bool,
+                 principal_investigator_display_name: str,
+                 data_steward_display_name: str,
                  responsible_cost_center: str,
                  storage_quota_gb: int,
-                 data_steward: str):
-        self.project = project
-        self.resource: str = resource
+                 size: int,
+                 manager_users: Users,
+                 manager_groups: Groups,
+                 contributor_users: Users,
+                 contributor_groups: Groups,
+                 viewer_users: Users,
+                 viewer_groups: Groups):
+        self.id: str = id
         self.title: str = title
-        self.principle_investigator: str = principle_investigator
+        self.enable_open_access_export: bool = enable_open_access_export
+        self.enable_archive: bool = enable_archive
+        self.principal_investigator_display_name: str = principal_investigator_display_name
+        self.data_steward_display_name: str = data_steward_display_name
         self.responsible_cost_center: str = responsible_cost_center
         self.storage_quota_gb: int = storage_quota_gb
-        self.data_steward: str = data_steward
+        self.size: int = size
+        self.manager_users: Users = manager_users
+        self.manager_groups: Groups = manager_groups
+        self.contributor_users: Users = contributor_users
+        self.contributor_groups: Groups = contributor_groups
+        self.viewer_users: Users = viewer_users
+        self.viewer_groups: Groups = viewer_groups
 
     @classmethod
-    def create_from_rule_result(cls, result: Dict):
-        project = cls(result["project"],
-                      result["resource"],
-                      result["title"],
-                      result["principalInvestigator"],
-                      result["respCostCenter"],
-                      result["storageQuotaGiB"],
-                      result["dataSteward"])
-        return project
+    def create_from_rule_result(cls, result: Dict) -> 'ProjectData':
+        project_details = cls(result["project"],
+                              result["title"],
+                              result["enableOpenAccessExport"],
+                              result["enableArchive"],
+                              result["principalInvestigatorDisplayName"],
+                              result["dataStewardDisplayName"],
+                              result["respCostCenter"],
+                              result["storageQuotaGiB"],
+                              result["dataSizeGiB"],
+                              Users.create_from_rule_result(result['managers']['userObjects']),
+                              Groups.create_from_rule_result(result['managers']['groupObjects']),
+                              Users.create_from_rule_result(result['contributors']['userObjects']),
+                              Groups.create_from_rule_result(result['contributors']['groupObjects']),
+                              Users.create_from_rule_result(result['viewers']['userObjects']),
+                              Groups.create_from_rule_result(result['viewers']['groupObjects']))
+        return project_details
--- tests/test_project.py
+++ tests/test_project.py
@@ -1,44 +1,31 @@
 from irodsrulewrapper.dto.project import Project
 from irodsrulewrapper.dto.projects import Projects
-from irodsrulewrapper.dto.project_data import ProjectData
 import json
 
+
+
+
+def test_project():
+    project_details = Project.create_from_rule_result(json.loads(PROJECT_JSON))
+    assert project_details is not None
+    assert project_details.manager_users.users[0].display_name == 'test_manager'
+    assert project_details.contributor_users.users[0].display_name == 'test_contributor'
+    assert project_details.viewer_users.users[0].display_name == 'test_viewer'
+    assert project_details.title == 'test_title'
+    assert project_details.enable_open_access_export is False
+    assert project_details.enable_archive is True
+    assert project_details.size == 99
+
+
+def test_projects():
+    projects = Projects.create_from_rule_result(json.loads(PROJECTS_JSON))
+    assert projects is not None
+    assert projects.projects.__len__() == 2
+    assert projects.projects[0].title == "test_title"
+    assert projects.projects[1].title == "test_title2"
+
+
 PROJECT_JSON = '''
-{
-    "project": "test_project",
-    "resource": "test_resource",
-    "title": "test_title",
-    "principalInvestigator": "test_pi",
-    "respCostCenter": "test_cost",
-    "storageQuotaGiB": 99,
-    "dataSteward": "test_datasteward"
-}
-'''
-
-PROJECTS_JSON = '''
-[
-{
-    "project": "test_project",
-    "resource": "test_resource",
-    "title": "test_title",
-    "principalInvestigator": "test_pi",
-    "respCostCenter": "test_cost",
-    "storageQuotaGiB": 99,
-    "dataSteward": "test_datasteward"
-},
-{
-    "project": "test_project2",
-    "resource": "test_resource2",
-    "title": "test_title2",
-    "principalInvestigator": "test_pi2",
-    "respCostCenter": "test_cost2",
-    "storageQuotaGiB": 11,
-    "dataSteward": "test_datasteward2"
-}
-]
-'''
-
-PROJECT_DETAILS_JSON = '''
 {
     "project": "test_project",
     "title": "test_title",
@@ -50,7 +37,7 @@ PROJECT_DETAILS_JSON = '''
     "storageQuotaGiB": 11,
     "dataSizeGiB": 99,
     "managers": {
-        "userObjects": 
+        "userObjects":
         [
             {
                 "userName": "test_manager",
@@ -68,7 +55,7 @@ PROJECT_DETAILS_JSON = '''
         ]
     },
     "contributors": {
-        "userObjects": 
+        "userObjects":
         [
             {
                 "userName": "test_contributor",
@@ -86,7 +73,7 @@ PROJECT_DETAILS_JSON = '''
         ]
     },
     "viewers": {
-        "userObjects": 
+        "userObjects":
         [
             {
                 "userName": "test_viewer",
@@ -106,34 +93,139 @@ PROJECT_DETAILS_JSON = '''
 }
 '''
 
+PROJECTS_JSON = '''
+[
+{
+    "project": "test_project",
+    "title": "test_title",
+    "enableOpenAccessExport": false,
+    "enableArchive": true,
+    "principalInvestigatorDisplayName": "test_pi",
+    "dataStewardDisplayName": "test_datasteward",
+    "respCostCenter": "test_cost2",
+    "storageQuotaGiB": 11,
+    "dataSizeGiB": 99,
+    "managers": {
+        "userObjects":
+        [
+            {
+                "userName": "test_manager",
+                "displayName": "test_manager",
+                "userId": "0"
+            }
+        ],
+        "groupObjects": [
+            {
+                "groupName": "test_manager_group",
+                "groupId": "0",
+                "displayName": "Suppers en co",
+                "description": "some more details here"
+            }
+        ]
+    },
+    "contributors": {
+        "userObjects":
+        [
+            {
+                "userName": "test_contributor",
+                "displayName": "test_contributor",
+                "userId": "1"
+            }
+        ],
+        "groupObjects": [
+            {
+                "groupName": "test_contributor_group",
+                "groupId": "1",
+                "displayName": "Suppers en co",
+                "description": "some more details here"
+            }
+        ]
+    },
+    "viewers": {
+        "userObjects":
+        [
+            {
+                "userName": "test_viewer",
+                "displayName": "test_viewer",
+                "userId": "2"
+            }
+        ],
+        "groupObjects": [
+            {
+                "groupName": "test_viewer_group",
+                "groupId": "2",
+                "displayName": "Suppers en co",
+                "description": "some more details here"
+            }
+        ]
+    }
+},
+{
+    "project": "test_project2",
+    "title": "test_title2",
+    "enableOpenAccessExport": false,
+    "enableArchive": true,
+    "principalInvestigatorDisplayName": "test_pi2",
+    "dataStewardDisplayName": "test_datasteward2",
+    "respCostCenter": "test_cost2",
+    "storageQuotaGiB": 11,
+    "dataSizeGiB": 99,
+    "managers": {
+        "userObjects":
+        [
+            {
+                "userName": "test_manager",
+                "displayName": "test_manager",
+                "userId": "0"
+            }
+        ],
+        "groupObjects": [
+            {
+                "groupName": "test_manager_group",
+                "groupId": "0",
+                "displayName": "Suppers en co",
+                "description": "some more details here"
+            }
+        ]
+    },
+    "contributors": {
+        "userObjects":
+        [
+            {
+                "userName": "test_contributor",
+                "displayName": "test_contributor",
+                "userId": "1"
+            }
+        ],
+        "groupObjects": [
+            {
+                "groupName": "test_contributor_group",
+                "groupId": "1",
+                "displayName": "Suppers en co",
+                "description": "some more details here"
+            }
+        ]
+    },
+    "viewers": {
+        "userObjects":
+        [
+            {
+                "userName": "test_viewer",
+                "displayName": "test_viewer",
+                "userId": "2"
+            }
+        ],
+        "groupObjects": [
+            {
+                "groupName": "test_viewer_group",
+                "groupId": "2",
+                "displayName": "Suppers en co",
+                "description": "some more details here"
+            }
+        ]
+    }
+}
+]
+'''
 
-def test_project():
-    project = Project.create_from_rule_result(json.loads(PROJECT_JSON))
-    assert project is not None
-    assert project.project == "test_project"
-    assert project.resource == "test_resource"
-    assert project.title == "test_title"
-    assert project.principle_investigator == "test_pi"
-    assert project.responsible_cost_center == "test_cost"
-    assert project.storage_quota_gb == 99
-    assert project.data_steward == "test_datasteward"
 
-
-def test_projects():
-    projects = Projects.create_from_rule_result(json.loads(PROJECTS_JSON))
-    assert projects is not None
-    assert projects.projects.__len__() == 2
-    assert projects.projects[0].title == "test_title"
-    assert projects.projects[1].title == "test_title2"
-
-
-def test_project_details():
-    project_details = ProjectData.create_from_rule_result(json.loads(PROJECT_DETAILS_JSON))
-    assert project_details is not None
-    assert project_details.manager_users.users[0].display_name == 'test_manager'
-    assert project_details.contributor_users.users[0].display_name == 'test_contributor'
-    assert project_details.viewer_users.users[0].display_name == 'test_viewer'
-    assert project_details.title == 'test_title'
-    assert project_details.enable_open_access_export is False
-    assert project_details.enable_archive is True
-    assert project_details.size == 99

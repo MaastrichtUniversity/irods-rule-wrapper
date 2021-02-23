9f2042738206002086bb408d75840e173aabdf31
JonathanMELIUS <meliusjonathan@gmail.com>
2021-02-23 10:17:20 +0100

    Update ManagingProjects dto [DHS-1144, DHS-1143]

--- irodsrulewrapper/dto/managing_projects.py
+++ irodsrulewrapper/dto/managing_projects.py
@@ -1,8 +1,10 @@
 class ManagingProjects:
-    def __init__(self, managers, contributors, viewers):
+    def __init__(self, managers, contributors, viewers, principal_investigator, data_steward):
         self.managers = managers
         self.contributors = contributors
         self.viewers = viewers
+        self.principal_investigator = principal_investigator
+        self.data_steward = data_steward
 
     @classmethod
     def create_from_rule_result(cls, result):
@@ -13,6 +15,6 @@ class ManagingProjects:
         managers = result["managers"]["users"]
         contributors = result["contributors"]["users"] + result["contributors"]["groups"]
         viewers = result["viewers"]["users"] + result["viewers"]["groups"]
-        projects = cls(managers, contributors, viewers)
+        projects = cls(managers, contributors, viewers, result["principal_investigator"], result["data_steward"])
 
         return projects

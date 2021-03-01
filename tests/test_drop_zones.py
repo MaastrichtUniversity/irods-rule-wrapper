c551d0105b8bf1c5d24676d5269cb8da3b6ae42d
Dean Linssen <d.linssen@maastrichtuniversity.nl>
2021-03-01 15:32:13 +0100

    Added rulewrapper call to generate token [DHS-1218, DHS-1310]

--- tests/test_drop_zones.py
+++ tests/test_drop_zones.py
@@ -7,3 +7,7 @@ import json
 def test_rule_get_groups():
     result = RuleManager().get_active_drop_zones("false")
     assert result is not None
+
+def test_generate_token():
+    result = RuleManager().generate_token()
+    assert result is not None and result.token is not None

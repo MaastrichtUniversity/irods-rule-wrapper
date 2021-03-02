71494ab07566c3b692dbcf7c22517970fc98ed59
Dean Linssen <d.linssen@maastrichtuniversity.nl>
2021-03-02 14:12:35 +0100

    Changes to ContributingProject dto [DHS-1218]

--- irodsrulewrapper/dto/contributing_project.py
+++ irodsrulewrapper/dto/contributing_project.py
@@ -2,18 +2,24 @@ from typing import List, Dict
 
 
 class ContributingProject:
-    def __init__(self, id: str, title: str, managers: List[str], contributors: List[str], viewers: List[str]):
+    def __init__(self, id: str, title: str, managers: List[str], contributors_users: List[str],
+                 contributors_groups: List[str], viewers_users: List[str], viewers_groups: List[str]):
         self.id: str = id
         self.title: str = title
         self.managers: List[str] = managers
-        self.contributors: List[str] = contributors
-        self.viewers: List[str] = viewers
+        self.contributors_users: List[str] = contributors_users
+        self.contributors_groups: List[str] = contributors_groups
+        self.viewers_users: List[str] = viewers_users
+        self.viewers_groups: List[str] = viewers_groups
 
     @classmethod
     def create_from_rule_result(cls, result: Dict) -> 'ManagingProjects':
         managers = result["managers"]["users"]
-        contributors = result["contributors"]["users"] + result["contributors"]["groups"]
-        viewers = result["viewers"]["users"] + result["viewers"]["groups"]
-        projects = cls(result["id"], result["title"], managers, contributors, viewers)
+        contributors_users = result["contributors"]["users"]
+        contributors_groups = result["contributors"]["groups"]
+        viewers_users = result["viewers"]["users"]
+        viewers_groups = result["viewers"]["groups"]
+        projects = cls(result["id"], result["title"], managers, contributors_users, contributors_groups, viewers_users,
+                       viewers_groups)
 
         return projects

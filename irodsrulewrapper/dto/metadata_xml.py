dc77e3076f9f5b6b1d4cdeb74de26deadca1f9e0
JonathanMELIUS <meliusjonathan@gmail.com>
2021-03-05 14:22:32 +0100

    Update MetadataXML with OLS attributes [DHS-1218, DHS-1302, DHS-1303, DHS-1304]

--- irodsrulewrapper/dto/metadata_xml.py
+++ irodsrulewrapper/dto/metadata_xml.py
@@ -12,7 +12,14 @@ class MetadataXML:
                  title: str,
                  description: str,
                  date: str,
-                 articles: str):
+                 articles: str,
+                 organism_id: str,
+                 organism_label: str,
+                 tissue_id: str,
+                 tissue_label: str,
+                 technology_id: str,
+                 technology_label: str):
+
         self.creator: str = creator
         self.token: str = token
         self.project: str = project
@@ -20,11 +27,21 @@ class MetadataXML:
         self.description: str = description
         self.date: str = date
         self.articles: str = articles
+        self.organism_id: str = organism_id
+        self.organism_label: str = organism_label
+        self.tissue_id: str = tissue_id
+        self.tissue_label: str = tissue_label
+        self.technology_id: str = technology_id
+        self.technology_label: str = technology_label
 
     @classmethod
     def create_from_dict(cls, data: Dict) -> 'MetadataXML':
-        metadata = cls(data["creator"], data["token"], data["project"],
-                       data["title"], data["description"], data["date"], data["articles"])
+        metadata = cls(data["creator"], data["token"],
+                       data["project"], data["title"], data["description"],
+                       data["date"], data["articles"],
+                       data["organism_id"], data["organism_label"],
+                       data["tissue_id"], data["tissue_label"],
+                       data["technology_id"], data["technology_label"])
         return metadata
 
     def write_metadata_xml(self, session):
@@ -66,6 +83,21 @@ class MetadataXML:
 
         medata_xml = ET.fromstring(xml)
 
+        if self.organism_label != '':
+            organism = medata_xml.find("organism")
+            organism.set("id", self.organism_id)
+            organism.text = self.organism_label
+
+        if self.tissue_label != '':
+            tissue = medata_xml.find("tissue")
+            tissue.set("id", self.tissue_id)
+            tissue.text = self.tissue_label
+
+        if self.technology_label != '':
+            technology = medata_xml.find("technology")
+            technology.set("id", self.technology_id)
+            technology.text = self.technology_label
+
         for article in self.articles.split(","):
             url = "https://doi.org/" + article
             medata_xml.append(ET.fromstring("<article>" + url + "</article>"))
@@ -93,6 +125,9 @@ class MetadataXML:
 
         try:
             root = ET.fromstring(metadata_xml)
+            tissue = read_tag(root, "tissue")
+            technology = read_tag(root, "technology")
+            organism = read_tag(root, "organism")
             data = {
                 "project": root.find("project").text,
                 "title": root.find("title").text,
@@ -101,10 +136,20 @@ class MetadataXML:
                 "creator": root.find("creator").text,
                 "token": token,
                 "articles": read_tag_list(root, "article"),
+                "organism_id": organism['id'],
+                "organism_label": organism['label'],
+                "tissue_id": tissue['id'],
+                "tissue_label": tissue['label'],
+                "technology_id": technology['id'],
+                "technology_label": technology['label']
             }
 
-            return cls(data["creator"], data["token"], data["project"],
-                       data["title"], data["description"], data["date"], data["articles"])
+            return cls(data["creator"], data["token"],
+                       data["project"], data["title"], data["description"],
+                       data["date"], data["articles"],
+                       data["organism_id"], data["organism_label"],
+                       data["tissue_id"], data["tissue_label"],
+                       data["technology_id"], data["technology_label"])
 
         except ET.ParseError:
             # logger.warning("ProjectCollection %s/%s has invalid metadata.xml" % (project, collection))
@@ -120,3 +165,12 @@ def read_tag_list(root, tag):
     return concatenation[1:]
 
 
+def read_tag(root, tag):
+    if root.find(tag).text is not None:
+        # Check if the xml tag exist and if it contains an ontology class
+        if root.find(tag).get("id") is not None and ":http:" in root.find(tag).get("id"):
+            return {"id": root.find(tag).get("id"), "label": root.find(tag).text}
+        else:
+            return {"id": "", "label": root.find(tag).text}
+    else:
+        return {"id": "", "label": ""}

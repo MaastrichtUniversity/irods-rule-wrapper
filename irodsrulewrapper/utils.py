4f2cba07e3a13a439c777dde17505f9053770940
JonathanMELIUS <meliusjonathan@gmail.com>
2021-03-30 15:28:39 +0200

    Split RuleManager's methods into rule_managers.* [DHS-1384]

--- irodsrulewrapper/utils.py
+++ irodsrulewrapper/utils.py
@@ -1,3 +1,5 @@
+from irods.session import iRODSSession
+
 import re
 import os
 
@@ -34,3 +36,37 @@ def check_project_collection_path_format(path):
 def is_safe_path(basedir, path):
     match_path = os.path.abspath(path)
     return basedir == os.path.commonpath((basedir, match_path))
+
+
+class BaseRuleManager:
+    def __init__(self, client_user=None):
+        if client_user is None:
+            self.session = iRODSSession(host=os.environ['IRODS_HOST'], port=1247, user=os.environ['IRODS_USER'],
+                                        password=os.environ['IRODS_PASS'], zone='nlmumc')
+        else:
+            self.session = iRODSSession(host=os.environ['IRODS_HOST'], port=1247, user=os.environ['IRODS_USER'],
+                                        password=os.environ['IRODS_PASS'], zone='nlmumc', client_user=client_user)
+
+
+class RuleInputValidationError(Exception):
+    """Exception raised for errors during the rule's validation of the input parameters.
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return "RuleInputValidationError, {0}".format(self.message)
+
+
+class RuleInfo:
+    def __init__(self, name, get_result, session, dto, input_params=None, rule_body=None):
+        self.name = name
+        self.get_result = get_result
+        self.session = session
+        self.dto = dto
+        self.input_params = input_params
+        self.rule_body = rule_body

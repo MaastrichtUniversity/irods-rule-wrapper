6b41bb0d76bac6f9179810afa90aafda4d884c36
Luis Sago <lws.sago@gmail.com>
2021-07-30 09:38:46 +0200

    [DHS-1682]: add support for tapeUnarchive AVU

--- irodsrulewrapper/rule_managers/projects.py
+++ irodsrulewrapper/rule_managers/projects.py
@@ -194,7 +194,7 @@ class ProjectRuleManager(BaseRuleManager):
     @rule_call
     def create_new_project(self, authorizationPeriodEndDate, dataRetentionPeriodEndDate,
                            ingestResource, resource, storageQuotaGb, title, principalInvestigator,
-                           dataSteward, respCostCenter, openAccess, tapeArchive):
+                           dataSteward, respCostCenter, openAccess, tapeArchive, tapeUnarchive):
         """
         Create a new iRODS project
 
@@ -221,6 +221,8 @@ class ProjectRuleManager(BaseRuleManager):
         openAccess : str
             'true'/'false' excepted values
         tapeArchive : str
+            'true'/'false' excepted values
+        tapeUnarchive : str
             'true'/'false' excepted values
 
         Returns
@@ -262,6 +264,9 @@ class ProjectRuleManager(BaseRuleManager):
 
         if tapeArchive != "false" and tapeArchive != "true":
             raise RuleInputValidationError("invalid value for *tapeArchive: expected 'true' or 'false'")
+
+        if tapeUnarchive != "false" and tapeUnarchive != "true":
+            raise RuleInputValidationError("invalid value for *tapeUnarchive: expected 'true' or 'false'")
 
         return RuleInfo(name="create_new_project", get_result=True, session=self.session, dto=CreateProject)
 

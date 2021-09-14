e3fe6f7207f7424dec63050033db8be1e789a9c0
Dean Linssen <d.linssen@maastrichtuniversity.nl>
2021-09-14 15:40:30 +0200

    Added relative size to CollectionSize DTO [DHS-1769, DHS-1793]

--- irodsrulewrapper/dto/collection_size.py
+++ irodsrulewrapper/dto/collection_size.py
@@ -2,13 +2,14 @@ from typing import List, Dict
 
 
 class CollectionSize:
-    def __init__(self, collection: str, resource: str, resource_attr: str, size: str):
+    def __init__(self, collection: str, resource: str, resource_attr: str, size: str, relative_size: str):
         self.collection: str = collection
         self.resource: str = resource
         self.resource_attr: str = resource_attr
         self.size: str = size
+        self.relative_size: str = relative_size
 
     @classmethod
     def create_from_rule_result(cls, result: Dict) -> 'CollectionSize':
-        collection_size = cls(result["collection"], result["resource"], result["resourceAttr"], result["size"])
+        collection_size = cls(result["collection"], result["resource"], result["resourceAttr"], result["size"], result["relative_size"])
         return collection_size

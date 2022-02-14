0aa78260bb42ce34f618c8d3b74c36efbe095143
JonathanMELIUS <meliusjonathan@gmail.com>
2022-02-14 16:16:24 +0100

    Add source_collection parameter to create_ingest_metadata_versions [DHS-2066, DHS-2129]

--- irodsrulewrapper/rule_managers/ingest.py
+++ irodsrulewrapper/rule_managers/ingest.py
@@ -293,7 +293,7 @@ class IngestRuleManager(BaseRuleManager):
         return RuleInfo(name="get_versioned_pids", get_result=True, session=self.session, dto=MetadataPID)
 
     @rule_call
-    def create_ingest_metadata_versions(self, project_id, collection_id, source_collection):
+    def create_ingest_metadata_versions(self, project_id, collection_id, source_collection, overwrite_flag):
         """
         Create a snapshot of the collection metadata files (schema & instance):
             * Check if the snapshot folder (.metadata_versions) already exists, if not create it
@@ -307,6 +307,8 @@ class IngestRuleManager(BaseRuleManager):
             The collection where the instance.json is to fill (e.g: C000000002)
         source_collection: str
             The drop-zone absolute path (e.g: /nlmumc/ingest/zones/crazy-frog)
+        overwrite_flag: str
+            'true'/'false' expected; If true, the copy overwrites possible existing schema.1.json & instance.1.json files
         """
         if not check_project_id_format(project_id):
             raise RuleInputValidationError("invalid project's path format: e.g P000000010")
@@ -314,5 +316,7 @@ class IngestRuleManager(BaseRuleManager):
             raise RuleInputValidationError("invalid collection id; eg. C000000001")
         if not isinstance(source_collection, str):
             raise RuleInputValidationError("invalid type for *source_collection: expected a string")
+        if overwrite_flag != "false" and overwrite_flag != "true":
+            raise RuleInputValidationError("invalid value for *overwrite_flag: expected 'true' or 'false'")
 
         return RuleInfo(name="create_ingest_metadata_versions", get_result=False, session=self.session, dto=None)

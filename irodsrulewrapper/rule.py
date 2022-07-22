70589d6d3d0af4ab4431a420b7d9bff48b9e7719
Daniel Theunissen <d.theunissen@maastrichtuniversity.nl>
2022-07-22 13:36:52 +0200

    Remove some unused print statements [DHS-2460, DHS-2459, DHS-2250]

--- irodsrulewrapper/rule.py
+++ irodsrulewrapper/rule.py
@@ -64,7 +64,6 @@ class RuleManager(
         try:
             query = SpecificQuery(self.session, alias="delete_password", args=[irods_id])
             result = query.execute()
-            # print(f"result: {result}")
             if result and result[0] != 0:
                 raise QueryException  # TODO maybe also log an error for elastalert??
         except CAT_NO_ROWS_FOUND:
@@ -73,9 +72,7 @@ class RuleManager(
     def count_user_temporary_passwords(self, irods_id):
         try:
             query = SpecificQuery(self.session, alias="count_password", args=[irods_id])
-            # print(f"query: {query}")
             for result in query:
-                # print(f"result: {result}")
                 return result[0]
         except CAT_NO_ROWS_FOUND:
             return 0
@@ -83,9 +80,7 @@ class RuleManager(
     def get_user_temporary_password_creation_timestamp(self, irods_id):
         try:
             query = SpecificQuery(self.session, alias="get_create_ts_password", args=[irods_id])
-            # print(f"query: {query}")
             for result in query:
-                # print(f"result: {result}")
                 return result[0]
         except CAT_NO_ROWS_FOUND:
             return 0

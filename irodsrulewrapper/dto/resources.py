7f22b6bb3f813b9768f4fee5e01d38d6e7343151
JonathanMELIUS <meliusjonathan@gmail.com>
2022-08-11 15:30:44 +0200

    Add create_from_mock_result to Resources [no ci]

--- irodsrulewrapper/dto/resources.py
+++ irodsrulewrapper/dto/resources.py
@@ -1,4 +1,6 @@
 """This module contains the Resources DTO class and its factory constructor."""
+import json
+
 from irodsrulewrapper.dto.resource import Resource
 
 
@@ -16,3 +18,22 @@ class Resources:
             output.append(resource)
         resources = cls(output)
         return resources
+
+    @classmethod
+    def create_from_mock_result(cls, mock_json=None) -> "Resources":
+        if mock_json is None:
+            mock_json = cls.MOCK_JSON
+        return cls.create_from_rule_result(json.loads(mock_json))
+
+    MOCK_JSON = """
+    [
+        {
+            "comment": "Replicated-resource-for-UM",
+            "name": "replRescUM01"
+        },
+        {
+            "comment": "Replicated-resource-for-AZM",
+            "name": "replRescAZM01"
+        }
+    ]    
+    """

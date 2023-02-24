869080131d13d9ed27762835c26d2401b220fae8
jonathanMELIUS <meliusjonathan@gmail.com>
2023-02-24 10:39:38 +0100

    Update ActiveProcess(es) & DropZone(s) DTOs [DHS-2117, DHS-2911]

--- irodsrulewrapper/dto/active_processes.py
+++ irodsrulewrapper/dto/active_processes.py
@@ -1,24 +1,18 @@
 """This module contains the ActiveProcesses DTO class and its factory constructor."""
 from irodsrulewrapper.dto.active_proces import ActiveProcess
 from irodsrulewrapper.dto.drop_zone import DropZone
+from pydantic import BaseModel
 
 
-class ActiveProcesses:
+class ActiveProcesses(BaseModel):
     """
     This class represents a list of iRODS active data transfer processes (ingest, tape archive & DataverseNL export).
     """
 
-    def __init__(
-        self,
-        completed: list[DropZone | ActiveProcess],
-        error: list[DropZone | ActiveProcess],
-        in_progress: list[DropZone | ActiveProcess],
-        open: list[DropZone],
-    ):
-        self.completed: list[DropZone | ActiveProcess] = completed
-        self.error: list[DropZone | ActiveProcess] = error
-        self.in_progress: list[DropZone | ActiveProcess] = in_progress
-        self.open: list[DropZone] = open
+    completed: list[DropZone | ActiveProcess]
+    error: list[DropZone | ActiveProcess]
+    in_progress: list[DropZone | ActiveProcess]
+    open: list[DropZone]
 
     @classmethod
     def create_from_rule_result(cls, result: dict) -> "ActiveProcesses":
@@ -35,15 +29,14 @@ class ActiveProcesses:
         for process in result["open"]:
             cls.parse_active_process(process, open_list)
 
-        output = cls(completed, error, in_progress, open_list)
+        output = cls(completed=completed, error=error, in_progress=in_progress, open=open_list)
 
         return output
 
     @staticmethod
     def parse_active_process(process: dict, process_state_list: list):
         if process["process_type"] == "drop_zone":
-            drop_zone = DropZone.create_from_rule_result(process)
-            process_state_list.append(drop_zone)
+            item = DropZone.create_from_rule_result(process)
         else:
-            export = ActiveProcess.create_from_rule_result(process)
-            process_state_list.append(export)
+            item = ActiveProcess.create_from_rule_result(process)
+        process_state_list.append(item)
--- irodsrulewrapper/dto/drop_zones.py
+++ irodsrulewrapper/dto/drop_zones.py
@@ -1,12 +1,12 @@
 """This module contains the DropZones class and its factory constructor."""
 from irodsrulewrapper.dto.drop_zone import DropZone
+from pydantic import BaseModel
 
 
-class DropZones:
+class DropZones(BaseModel):
     """This class represents a list of iRODS DropZones DTOs."""
 
-    def __init__(self, users: list["DropZone"]):
-        self.drop_zones: list["DropZone"] = users
+    drop_zones: list[DropZone]
 
     @classmethod
     def create_from_rule_result(cls, result: dict) -> "DropZones":
@@ -14,5 +14,6 @@ class DropZones:
         for item in result:
             drop_zone = DropZone.create_from_rule_result(item)
             output.append(drop_zone)
-        drop_zones = cls(output)
+        drop_zones = cls(drop_zones=output)
+
         return drop_zones

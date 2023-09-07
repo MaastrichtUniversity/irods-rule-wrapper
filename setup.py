f131b24a5f36a15598a7e45d20343edfcc28eff8
kristinegusta <kristine.guta@maastrichtuniversity.nl>
2023-09-07 14:39:52 +0200

    Change dh-python-irods version to use DHS-3024 branch

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     install_requires=[
         "python-irodsclient==1.1.6",
         "cedar-parsing-utils @ git+https://github.com/MaastrichtUniversity/cedar-parsing-utils.git@v1.0.0#egg=cedar-parsing-utils",
-        "dh-python-irods-utils @ git+https://github.com/MaastrichtUniversity/dh-python-irods-utils.git@v1.1.4#egg=dh-python-irods-utils",
+        "dh-python-irods-utils @ git+https://github.com/MaastrichtUniversity/dh-python-irods-utils.git@DHS-3024#egg=dh-python-irods-utils",
         "pika>=1.2.0,<2.0.0",
         "pytz>=2021.3",
         "pydantic>=1.9.1,<2.0.0",

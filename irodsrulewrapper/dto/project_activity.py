192cf82f3c8ffb14598bd08fc7463336dd4f0ff2
danieltheunissen <d.theunissen@maastrichtuniversity.nl>
2023-09-15 11:31:38 +0200

    Rename has_only_active_collection in ProjectActivity to has_active_collection [DHS-3024]

--- irodsrulewrapper/dto/project_activity.py
+++ irodsrulewrapper/dto/project_activity.py
@@ -7,13 +7,13 @@ class ProjectActivity(BaseModel):
     """This class represents the different activities related to a project. It is relevant for a project deletion"""
 
     has_process_activity: bool
-    has_only_active_collection: bool
+    has_active_collection: bool
 
     @classmethod
     def create_from_rule_result(cls, result: dict) -> "ProjectActivity":
         project_activity = cls(
             has_process_activity=result["has_process_activity"],
-            has_only_active_collection=result["has_only_active_collection"],
+            has_active_collection=result["has_active_collection"],
         )
 
         return project_activity
